6ceac6650364f322a40fc57181af89147c750c89
oiviana <vianalucas80@gmail.com>
2024-06-06 16:32:32 -0300

    feat: menu initial structure

---
+++ src/components/sections/Menu/DesktopMenu/index.tsx
@@ -0,0 +1,15 @@
+export default function DesktopMenu(){
+    return(
+        <div className="flex w-full h-12">
+            <div className="flex mx-auto max-[100rem] justify-center items-center gap-x-20">
+                <span>Novidades</span>
+                <span>Vestidos</span>
+                <span>Roupas</span>
+                <span>Sapatos</span>
+                <span>Lingerie</span>
+                <span>Acessórios</span>
+                <span>OUTLET</span>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/sections/Menu/MobileMenu/index.tsx
@@ -0,0 +1,12 @@
+import { IoMenu } from "react-icons/io5";
+
+export default function MobileMenu(){
+
+    return(
+        <div>
+            <button>
+            <IoMenu size={35}/>
+            </button>
+        </div>
+    )
+}
