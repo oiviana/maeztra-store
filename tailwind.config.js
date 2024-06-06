1b84683819f7c4003ca6def51f536c2d7e7faf6d
oiviana <vianalucas80@gmail.com>
2024-06-06 12:43:10 -0300

    config: theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-yellow':'#FAA500',
+        'primary-white':'#FFFFFF',
+        'secondary-white':'#FAFAFA',
+        'tertiary-white':'#EFEFEF',
+        'primary-gray':'#353535',
+        'primary-black':'#000000',
+
+      },
+    },
     fontFamily: {
       titillium: ["Titillium Web", "sans-serif"],
     }
