510748665b8c6be2ff78fdd1192dc0c8521b2617
oiviana <vianalucas80@gmail.com>
2024-06-07 13:21:09 -0300

    feat: footer

---
+++ src/components/Layout/Footer/index.tsx
@@ -0,0 +1,42 @@
+import { ImFacebook2 } from "react-icons/im";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import { FaCcVisa } from "react-icons/fa6";
+import { FaCcMastercard } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
+import VtexIcon from "../../icons/VtexIcon";
+import LogoFooter from "../../icons/LogoFooter";
+
+export default function Footer() {
+  return (
+    <footer className="bg-primary-gray w-full">
+      <div className="max-w-[100rem] mx-auto px-2 py-6 gap-7 lg:gap-0 h-[233px] lg:h-[85px] text-primary-white flex flex-col  lg:flex-row lg:justify-between items-center">
+        <div className="flex gap-4">
+          <ImFacebook2 size={32} />
+          <FaLinkedinIn size={32} />
+          <FaInstagram size={32} />
+          <FaYoutube size={32} />
+        </div>
+
+        <div className="flex gap-5">
+          <FaCcVisa size={32} />
+          <FaCcMastercard size={32} />
+          <FaCcVisa size={32} />
+          <FaCcMastercard size={32} />
+        </div>
+
+        <div className="flex gap-5">
+          <div className="flex flex-col gap-1">
+            <p className="text-[10px]">powered by</p>
+
+            <VtexIcon />
+          </div>
+          <div className="flex flex-col gap-1">
+            <p className="text-[10px]">Developed by</p>
+            <LogoFooter />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
