2bc52fd026ab35ce4ec070d316d9cf63afc25ad4
oiviana <vianalucas80@gmail.com>
2024-06-08 16:12:42 -0300

    feat: footer menu component

---
+++ src/components/sections/Menu/FooterMenu/index.tsx
@@ -0,0 +1,18 @@
+import { menuItemsMock } from "./mock";
+
+export default function FooterMenu() {
+  return (
+    <>
+      <div className="hidden lg:flex w-full max-w-[67rem] h-[290px]  mx-auto justify-between py-11 px-1">
+        {menuItemsMock.map((item) => (
+          <div className="flex flex-col gap-4">
+            <h3 className="text-base font-bold text-primary-gray mb-1">{item.title}</h3>
+            {item.menuItems.map((menuitem) => (
+              <a href={menuitem.url} className="text-[0.75rem] text-black">{menuitem.title}</a>
+            ))}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
---
+++ src/components/sections/Menu/FooterMenu/mock.ts
@@ -0,0 +1,53 @@
+export const menuItemsMock = [
+  {
+    title: "Informações",
+    menuItems:[
+      {
+        title:"Quem Somos",
+        url: "/"
+      },
+      {
+        title:"Prazo de Envio",
+        url: "/"
+      },
+      {
+        title:"Trocas e Devoluções",
+        url: "/"
+      },
+      {
+        title:"Promoções e Cupons",
+        url: "/"
+      },
+    ],
+  },
+  {
+    title: "Minha Conta",
+    menuItems:[
+      {
+        title:"Minha Conta",
+        url: "/"
+      },
+      {
+        title:"Meus Pedidos",
+        url: "/"
+      },
+      {
+        title:"Cadastre-se",
+        url: "/"
+      },
+    ],
+  },
+  {
+    title: "Onde nos Encontrar",
+    menuItems:[
+      {
+        title:"Lojas",
+        url: "/"
+      },
+      {
+        title:"Endereço",
+        url: "/"
+      },
+    ],
+  },
+];
