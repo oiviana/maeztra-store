dfc3b647772366bc8ad5a5f61733e9aa33713637
oiviana <vianalucas80@gmail.com>
2024-06-08 16:13:58 -0300

    feat: brands mock

---
+++ src/components/sections/Brands/mock.ts
@@ -0,0 +1,17 @@
+export const brandsMock = [
+  {
+    img: "/images/brands/comma.svg",
+  },
+  {
+    img: "/images/brands/melissa.svg",
+  },
+  {
+    img: "/images/brands/forever.svg",
+  },
+  {
+    img: "/images/brands/zara.svg",
+  },
+  {
+    img: "/images/brands/ann.svg",
+  },
+];
