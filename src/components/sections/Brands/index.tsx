de83d3f9199d86574024b61b8996b1e2b49f51c9
oiviana <vianalucas80@gmail.com>
2024-06-10 12:20:00 -0300

    fix: brands adjustments

--- src/components/sections/Brands/index.tsx
+++ src/components/sections/Brands/index.tsx
@@ -1,17 +1,16 @@
 import BrandCard from "./BrandCard";
 import { brandsMock } from "./mock";
 import Slider from "react-slick";
+import { sliderSettings } from "../Advantages/sliderSettings";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { sliderSettings } from "../Advantages/sliderSettings";
+
 
 export default function Brands() {
-  const brandCards = brandsMock.map((item) => (
-    <BrandCard image={item.img} />
-  ));
+  const brandCards = brandsMock.map((item) => <BrandCard image={item.img} />);
 
   return (
-    <div className="overflow-hidden pl-2 flex flex-col gap-4 my-6">
+    <div className="overflow-hidden pl-2 flex flex-col gap-4 my-8">
       <h2 className="mx-auto text-2xl lg:text-[2rem] text-primary-gray font-bold">
         Marcas Parceiras
       </h2>
